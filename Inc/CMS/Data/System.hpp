8abd506eed7ce6242361ed50175a30368d9e2633
jmaralo <martinezalonsojn@gmail.com>
2025-03-22 19:36:17 +0100

    Aggregate all modules values into arrays

--- Inc/CMS/Data/System.hpp
+++ Inc/CMS/Data/System.hpp
@@ -9,11 +9,59 @@ namespace CMS::Data {
 
 template <uint8_t NumberOfStrings, uint8_t NumberOfModules>
 struct System {
-    using String = std::array<Module, NumberOfModules>;
+    constexpr static uint8_t number_of_strings{NumberOfStrings - 1};
+    constexpr static uint8_t number_of_modules{NumberOfModules - 1};
 
-    std::array<String, NumberOfStrings> strings{};
+    using String = std::array<Module, number_of_modules>;
+
+    std::array<String, number_of_strings> strings{};
 
     float total_voltage_volts{0.0};
+
+    std::array<std::array<std::array<float *, 48>, number_of_modules>,
+               number_of_strings>
+        all_cells_voltage{};
+    std::array<std::array<float *, number_of_modules>, number_of_strings>
+        all_module_voltage{};
+    std::array<std::array<float *, number_of_modules>, number_of_strings>
+        all_max_cell_voltage{};
+    std::array<std::array<float *, number_of_modules>, number_of_strings>
+        all_min_cell_voltage{};
+    std::array<std::array<float *, number_of_modules>, number_of_strings>
+        all_avg_cell_voltage{};
+    std::array<std::array<float *, number_of_modules>, number_of_strings>
+        all_max_temperature{};
+    std::array<std::array<float *, number_of_modules>, number_of_strings>
+        all_min_temperature{};
+
+    System() {
+        for (uint8_t string{1}; string < NumberOfStrings; ++string) {
+            for (uint8_t module{1}; module < NumberOfModules; ++module) {
+                for (uint8_t cell{0}; cell < 48; ++cell) {
+                    all_cells_voltage[string][module][cell] =
+                        &strings[string][module].cell_voltages[cell].millivolts;
+                }
+
+                all_module_voltage[string][module] =
+                    &strings[string][module].module_voltage.volts;
+
+                all_max_cell_voltage[string][module] =
+                    &strings[string][module].max_cell_voltage.millivolts;
+
+                all_min_cell_voltage[string][module] =
+                    &strings[string][module].min_cell_voltage.millivolts;
+
+                all_avg_cell_voltage[string][module] =
+                    &strings[string][module].avg_cell_voltage.millivolts;
+
+                all_max_temperature[string][module] =
+                    &strings[string][module].max_temperature.celsius;
+
+                all_min_temperature[string][module] =
+                    &strings[string][module].min_temperature.celsius;
+            }
+        }
+    }
 };
 
 };  // namespace CMS::Data
